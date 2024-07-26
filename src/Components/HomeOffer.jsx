c5be36ae95f0074ece5b5fd5fcb210218b5b6d85
marc <marcgenecrisolo@gmail.com>
2024-07-26 09:59:15 +0800

    home offer

--- src/Components/HomeOffer.jsx
+++ src/Components/HomeOffer.jsx
@@ -1,18 +1,36 @@
 function HomeOffer() {
   return (
     <>
-      <div className="carousel carousel-end rounded-box mt-2">
-        <div className="carousel-item m-2">
+      <div className="carousel rounded-box mt-2 flex flex-wrap justify-center sm:carousel-end sm:flex-nowrap">
+        <div className="carousel-item m-2 relative">
           <img
-            src="https://www.blizzstatic.com/dynamicmedia/image/308/56706ecd41d98.jpg?w=454"
-            alt="Drink"
+            src="/images/homeoffer1.png"
+            alt="Drink 1"
+            className="w-96"
           />
+          <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-50 p-4">
+            <h3 className="text-white text-lg font-bold">BENTO</h3>
+          </div>
         </div>
-        <div className="carousel-item m-2">
+        <div className="carousel-item m-2 relative">
           <img
-            src="https://th.bing.com/th/id/OIP.yJ8btegWeZNkoppasEwlxQAAAA?w=474&h=316&rs=1&pid=ImgDetMain"
-            alt="Drink"
+            src="/images/homeoffer2.png"
+            alt="Drink 2"
+            className="w-96"
           />
+          <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-50 p-4">
+            <h3 className="text-white text-lg font-bold">RAMEN</h3>
+          </div>
+        </div>
+        <div className="carousel-item m-2 relative">
+          <img
+            src="/images/homeoffer3.png"
+            alt="Drink 3"
+            className="w-96"
+          />
+          <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-50 p-4">
+            <h3 className="text-white text-lg font-bold">DRINKS</h3>
+          </div>
         </div>
       </div>
     </>
