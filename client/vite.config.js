5b203ca5dce58696a08825e95f781913983d982e
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2024-07-26 18:05:12 +0800

    changed react port 3000

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // backend server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // remove /api prefix in the url
+      }
+    }
+  }
+  
 })
