84d778e0ce49206e78c8c340b4479c2141ef7dec
marc <marcgenecrisolo@gmail.com>
2024-08-08 16:25:49 +0800

    updated index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,43 +1,34 @@
 import express, { type NextFunction, type Request, type Response } from 'express';
 import { config } from './secret';
-import { authRoutes } from './routes/authRoutes'
+import { authRoutes } from './routes/authRoutes';
 import { userRoutes } from './routes/userRoutes';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-
-
+// Initialize the Express application
 const app = express();
 
-//api routes
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL encoded bodies
+
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
-
-
-//middlewares
-app.use(cors()); //Enable CORS
-app.use(bodyParser.json()); //Parse JSON bodies
-app.use(bodyParser.urlencoded({ extended: true }));//Parse URL encodede bodies
-
-
-//default route
-
+// Default route
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-//Global error handling middleware
+// Global error handling middleware
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!')
-})
+    res.status(500).send('Something broke!');
+});
 
-//server 
+// Start the server
 app.listen(config.PORT, () => {
     console.log(`Server is running on http://localhost:${config.PORT}`);
 });
-
-
-
-
